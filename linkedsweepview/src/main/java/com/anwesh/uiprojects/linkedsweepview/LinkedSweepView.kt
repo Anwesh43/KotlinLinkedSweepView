7b836b47eb124cabbd87df752da30cba2914e89a
Anwesh43 <anweshthecool0@gmail.com>
2018-07-01 10:10:59 +0530

    adding state points

--- linkedsweepview/src/main/java/com/anwesh/uiprojects/linkedsweepview/LinkedSweepView.kt
+++ linkedsweepview/src/main/java/com/anwesh/uiprojects/linkedsweepview/LinkedSweepView.kt
@@ -31,15 +31,21 @@ class LinkedSweepView(ctx : Context) : View(ctx) {
         return true
     }
 
-    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var scale : Float = 0f) {
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
+
+        val scales : Array<Float> = arrayOf(0f, 0f)
 
         fun update(stopcb : (Float) -> Unit) {
-            scale += dir * 0.1f
-            if (Math.abs(scale - prevScale) > 1) {
-                scale = prevScale + dir
-                dir = 0f
-                prevScale = scale
-                stopcb(prevScale)
+            scales[j] += dir * 0.1f
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += dir.toInt()
+                if (j == scales.size || j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
             }
         }
 
