a1214349b66678a12c95b067e54ba0d5dd2eb61e
Anwesh43 <anweshthecool0@gmail.com>
2018-07-01 10:31:10 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsweepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsweepview/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.anwesh.uiprojects.kotlinlinkedsweepview
 
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linkedsweepview.LinkedSweepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LinkedSweepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
