ab358dcacca84d61f8cb2b7dea9671c7445701f9
Anwesh43 <anweshthecool0@gmail.com>
2018-07-01 09:43:45 +0530

    importing classes required

--- linkedsweepview/src/main/java/com/anwesh/uiprojects/linkedsweepview/LinkedSweepView.kt
+++ linkedsweepview/src/main/java/com/anwesh/uiprojects/linkedsweepview/LinkedSweepView.kt
@@ -3,3 +3,9 @@ package com.anwesh.uiprojects.linkedsweepview
 /**
  * Created by anweshmishra on 01/07/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
