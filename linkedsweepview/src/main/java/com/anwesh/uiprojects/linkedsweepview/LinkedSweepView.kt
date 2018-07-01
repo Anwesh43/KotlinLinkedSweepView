6117266cc4aab8ec37d71d14b4a690398ead108b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-01 09:45:34 +0530

    adding LinkedSweepView class

--- linkedsweepview/src/main/java/com/anwesh/uiprojects/linkedsweepview/LinkedSweepView.kt
+++ linkedsweepview/src/main/java/com/anwesh/uiprojects/linkedsweepview/LinkedSweepView.kt
@@ -9,3 +9,21 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.Canvas
 import android.graphics.Paint
+
+class LinkedSweepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
