364bbada15bf1a42477993d5be6a80cd1d57f40c
Anwesh43 <anweshthecool0@gmail.com>
2018-07-01 10:36:53 +0530

    using CompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsweepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsweepview/MainActivity.kt
@@ -4,14 +4,18 @@ import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedsweepview.LinkedSweepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedSweepView.create(this)
+        val view : LinkedSweepView = LinkedSweepView.create(this)
         fullScreen()
+        view.addCompletionListener {
+            Toast.makeText(this, "sweeped for ${it + 1} time", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
